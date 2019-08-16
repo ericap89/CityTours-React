3d206dcf3a90d7bab0de36b083a279232cf3ba43
Erica P <erica.parra@rocketmail.com>
2019-08-16 13:03:14 -0700

    intial tourlist setup with intial basic funcitonality layout

---
+++ client/src/components/Tour/Tour.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./tour.scss"
+
+class Tour extends Component {
+    render() {
+        return (
+            <div>
+                <h1>tour
+                </h1>
+
+            </div>
+        );
+    }
+}
+
+export default Tour;
