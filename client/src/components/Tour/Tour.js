54a653c89db3a96eb11f1fb3c9fd3a9acb73d29a
Erica P <erica.parra@rocketmail.com>
2019-08-16 13:27:01 -0700

    tour component added w/ 1st round styling.

--- client/src/components/Tour/Tour.js
+++ client/src/components/Tour/Tour.js
@@ -3,12 +3,30 @@ import "./tour.scss"
 
 class Tour extends Component {
     render() {
-        return (
-            <div>
-                <h1>tour
-                </h1>
+        // console.log(this.props);
+        const {id, city, img, name, info} = this.props.tour;
+        const { removeTour} = this.props;
 
+        return (
+            
+            <article className="tour">
+            <div className="img-container">
+            <img src={img} alt="image of tour"/>
+            <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+            </span>
             </div>
+            <div className="tour-info">
+            <h3>{city}</h3>
+            <h4>{name}</h4>
+            <h5>info 
+                <span>
+                    <i className="fas fa-caret-square-down"></i>
+                </span>
+            </h5>
+            </div>
+            </article>
+
         );
     }
 }
